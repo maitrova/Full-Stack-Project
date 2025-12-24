9dee109da74a974a7dfe4d4a4a44222db95518c9
maitrova <maitrovavps@gmail.com>
2025-12-23 20:47:46 -0600

    changes added

--- frontend/src/components/usersavedesign.jsx
+++ frontend/src/components/usersavedesign.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { selectCurrentToken } from "../redux/slices/userSlice.js";
+import { selectCurrentToken } from "../redux/slices/Userslice.js";
 import {
   addToCart,
   selectCartItems,
