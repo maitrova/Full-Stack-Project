9a62247007b84c2f5d277b96c7baec23f324a5d2
Hemanth411-stack <nagalahemanth411@gmail.com>
2026-05-12 08:03:25 +0530

    added cat

--- frontend/src/utils/catalogueDesignRoutes.js
+++ frontend/src/utils/catalogueDesignRoutes.js
@@ -1,14 +1,3 @@
-const slugifyDesignSegment = (value = "") =>
-  String(value || "")
-    .trim()
-    .toLowerCase()
-    .replace(/&/g, " and ")
-    .replace(/[^a-z0-9]+/g, "-")
-    .replace(/^-+|-+$/g, "");
-
 export const buildCatalogueDesignPath = (design) => {
-  const slug = slugifyDesignSegment(design?.title || design?.productName || design?.name);
-  return slug ? `/catalogue/${slug}` : (design?._id ? `/catalogue/${design._id}` : null);
+  return design?._id ? `/catalogue/${design._id}` : null;
 };
-
-export { slugifyDesignSegment };
